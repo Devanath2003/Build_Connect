3330058efc7d26214f76e357069505154750136f
Devanath2003 <devanathskt@gmail.com>
2026-09-19 20:43:24 +0530

    password hashing info

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,8 +1,8 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from passlib.context import CryptContext
-from jose import JWTError, jwt
+from passlib.context import CryptContext #for password hashing.
+from jose import JWTError, jwt #JSON Object Signing and Encryption
 from datetime import datetime, timedelta, timezone
 from app.database import get_db
 from app.config import SECRET_KEY
