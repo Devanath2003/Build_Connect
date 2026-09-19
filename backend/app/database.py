d6bbe278c4185fc5dd9c98f027116a41762a8f66
Devanath2003 <devanathskt@gmail.com>
2026-09-19 19:20:03 +0530

    some info addition

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,5 @@
+# The whole process is for a level of abstraction.
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import DATABASE_URL
@@ -5,8 +7,20 @@ from app.config import DATABASE_URL
 # SQLAlchemy engine creation.
 engine = create_engine(DATABASE_URL)
 
+"""
+Engine - creates a connection pool with the postgres driver, tht means multiple connection links will be there, so if many requests come,
+and the engine will provide connections to each of them, and one request is completed , the connection is reused for another request
+"""
+
 # SessionLocal class for database sessions
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+"""
+SessionLocal - It is a Sessionmaking factory , where a session is a unit of work, which provided to an endpoint to access the
+database for a certain process - maybe for a query something, then after the work it is closed, so this Sessionmaker manages this session
+
+Generate and provide those who want a session with database or uses a connection from the engine when needed
+"""
 
 # Base class for the models to inherit from
 Base = declarative_base()
--- backend/app/routers/professionals.py
+++ backend/app/routers/professionals.py
@@ -8,3 +8,7 @@ from app.models.professional import ProfessionalProfile
 from app.schemas.professional import ProfessionalProfileCreate, ProfessionalProfileResponse
 
 router = APIRouter(prefix="/professionals",tags=["Professionals"])
+
+@router.post("/profile",response_model=ProfessionalProfileResponse)
+def create_professional_profile(profile: ProfessionalProfileCreate, current_user: User = Depends(get_current_user), db: Session = Depends(get_db),):
+    
